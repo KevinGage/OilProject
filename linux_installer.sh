360d018601a9ec7c690462a69ca1ef360a6f5e9e
Kevin Gage <dudeman02379@gmail.com>
2017-03-08 15:19:14 -0500

    fixed json syntax in created config file.  ref #2

--- linux_installer.sh
+++ linux_installer.sh
@@ -74,15 +74,15 @@ collectInformation ()
 	read checkTime
 	clear
 	
-	echo "SMTP server address: \"$smtpAddress\""
-	echo "SMTP server port: \"$smtpPort\""
-	echo "SMTP TLS: \"$smtpTLS\""
-	echo "Sender email: \"$senderAddress\""
-	echo "Sender password: \"$senderPassword\""
-	echo "Recipient email: \"$recipientAddress\""
-	echo "Price thrshold: \"$priceThreshold\""
-	echo "OccuranceCode (1: daily, 2 weekly, 3 monthly): \"$occuranceCode\""
-	echo "Schedule time of day: \"$checkTime\""
+	echo "SMTP server address: $smtpAddress"
+	echo "SMTP server port: $smtpPort"
+	echo "SMTP TLS: $smtpTLS"
+	echo "Sender email: $senderAddress"
+	echo "Sender password: $senderPassword"
+	echo "Recipient email: $recipientAddress"
+	echo "Price thrshold: $priceThreshold"
+	echo "OccuranceCode (1: daily, 2 weekly, 3 monthly): $occuranceCode"
+	echo "Schedule time of day: $checkTime"
 
 	read -p "Type y if everything looks correct." yn
     		case $yn in
@@ -109,12 +109,12 @@ createConfigFile ()
 {
 	#This writes the config file.  It does not currently set the correct permissions.
 	echo "module.exports = {" > ./config.js
-	echo "	\"smtpAddress\": $smtpAddress" >> ./config.js
-	echo "	\"smtpPort\": $smtpPort" >> ./config.js
-	echo "	\"smtpTLS\": $smtpTLS" >> ./config.js
-	echo "	\"senderAddress\": $senderAddress" >> ./config.js
-	echo "	\"senderPassword\": $senderPassword" >> ./config.js
-	echo "	\"recipientAddress\": $recipientAddress" >> ./config.js
+	echo "	\"smtpAddress\": \"$smtpAddress\"," >> ./config.js
+	echo "	\"smtpPort\": $smtpPort," >> ./config.js
+	echo "	\"smtpTLS\": \"$smtpTLS\"," >> ./config.js
+	echo "	\"senderAddress\": \"$senderAddress\"," >> ./config.js
+	echo "	\"senderPassword\": \"$senderPassword\"," >> ./config.js
+	echo "	\"recipientAddress\": \"$recipientAddress\"," >> ./config.js
 	echo "	\"priceThreshold\": $priceThreshold" >> ./config.js
 	echo "}" >> ./config.js
 }
