2d25c98f4f24b490ee50124cce2bf2211d85ce11
Kevin Gage <dudeman02379@gmail.com>
2017-03-16 13:55:34 -0400

    added node installer for ARMv6 platform.  This is for raspberry pi zero w.  ref #2

--- linux_installer.sh
+++ linux_installer.sh
@@ -29,9 +29,19 @@ promptNode6Install()
 
 installNode6 ()
 {
-        #This installs node 6 using apt https://nodejs.org/en/download/package-manager/
-        curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-        apt-get install -y nodejs
+	if grep --quiet ARMv6 /proc/cpuinfo; then
+  		#This downloads the node binaries for the ARM6 platform
+		wget https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-armv6l.tar.xz
+		tar -xf ./node-v6.10.0-linux-armv6l.tar.xz
+		cp ./node-v6.10.0-linux-armv6l/bin/* /bin/
+		rm ./node-v6.10.0-linux-armv6l.tar.xz
+		rm -rf ./node-v6.10.0-linux-armv6l
+	else
+        	#This installs node 6 using apt https://nodejs.org/en/download/package-manager/
+		curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+		apt-get install -y nodejs
+	fi		
+
 }
 
 checkPrerequisites ()
